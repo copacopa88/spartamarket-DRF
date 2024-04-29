0a34809dd2ca0540db5ae782d0180ca599d62331
Yosi <lyt2006@naver.com>
2024-04-29 16:41:50 +0900

    1차 수정

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    name = models.EmailField(max_length=20)
+    email = models.EmailField(max_length=255, unique=True)
+    birthday=models.DateField(auto_now=False, auto_now_add=False)
+    sex=models.CharField(max_length=20)
+    introduce=models.CharField(max_length=200)
+    
