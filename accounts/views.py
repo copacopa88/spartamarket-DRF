79810a93f4424e11c5507f4c52ef7aa940683931
Yosi <lyt2006@naver.com>
2024-05-01 17:26:53 +0900

    프로필 추가 수정

--- accounts/views.py
+++ accounts/views.py
@@ -5,15 +5,15 @@ from .serializers import RegisterSerializer,ProfileSerializer
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from .permissions import CustomReadOnly
+
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     
 
-class ProfileView(generics.RetrieveAPIView):
-    permission_classes = [IsAuthenticated]
+class ProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = ProfileSerializer
-    queryset = User.objects.all()
-    
-
+    queryset = User.objects.filter(username=['username'])
+    permission_classes = [CustomReadOnly]
