d7e3af987c8018b6e6e1c734438560c1dff2773f
Yosi <lyt2006@naver.com>
2024-05-03 00:40:06 +0900

    탈퇴 구현, 로그아웃 구?현, PAGINATION구

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,9 @@
 # users/views.py
-from django.shortcuts import get_object_or_404
 from rest_framework import status
 from .models import User
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer,ProfileSerializer
 from rest_framework.permissions import IsAuthenticated
 
